3352a7e8bb372654ba3f478d10060cb1da29c291
Mehmet Emre ÖZ <nyz_memre_09@hotmail.com>
2022-01-08 20:17:41 +0300

    added text_model.g.dart

--- lib/model/text_model.dart
+++ lib/model/text_model.dart
@@ -1,5 +1,12 @@
+import 'package:hive/hive.dart';
+
+part 'text_model.g.dart';
+
+@HiveType(typeId: 0)
 class TextModel {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String description;
 
   TextModel(this.title, this.description);
