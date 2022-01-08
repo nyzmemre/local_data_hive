0bc666a31a1760ab6f6e67859b96df0993323f6e
Mehmet Emre ÖZ <nyz_memre_09@hotmail.com>
2022-01-08 15:07:18 +0300

    change view model list items

--- lib/view_model/magazine_view_model.dart
+++ lib/view_model/magazine_view_model.dart
@@ -2,11 +2,11 @@ import 'package:local_data_hive/model/text_model.dart';
 
 class MagazineViewModel{
   List<TextModel> _magazineViewModel=[
-    TextModel("Spor Başlık-1", "Spor Açıklama-1"),
-    TextModel("Spor Başlık-2", "Spor Açıklama-2"),
-    TextModel("Spor Başlık-3", "Spor Açıklama-3"),
-    TextModel("Spor Başlık-4", "Spor Açıklama-4"),
-    TextModel("Spor Başlık-5", "Spor Açıklama-5"),
+    TextModel("Magazine Başlık-1", "Magazine Açıklama-1"),
+    TextModel("Magazine Başlık-2", "Magazine Açıklama-2"),
+    TextModel("Magazine Başlık-3", "Magazine Açıklama-3"),
+    TextModel("Magazine Başlık-4", "Magazine Açıklama-4"),
+    TextModel("Magazine Başlık-5", "Magazine Açıklama-5"),
   ];
 
   List<TextModel> get magazineViewModel=>_magazineViewModel;
